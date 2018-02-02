41e641e5e15c257571fcdeeda6a3c1152dd1ddb2
brminnick <brandon.r.minnick@gmail.com>
2018-02-02 14:27:18 -0800

    First Commit

---
+++ CustomStepper/App.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace CustomStepper
+{
+    public class App : Application
+    {
+        public App() => MainPage = new StepperPage();
+    }
+
+    class StepperPage : ContentPage
+    {
+        public StepperPage()
+        {
+            Content = new RedStepper
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+        }
+    }
+
+    public class RedStepper : Stepper
+    {
+        
+    }
+}
